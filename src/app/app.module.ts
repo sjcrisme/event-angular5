0959505c371d3d031f1a452e06f4b81f668413a0
Serhii Verbitskiy <sjcris.me@gmail.com>
2018-02-19 16:43:33 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { CheckerComponent } from './checker/checker.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckerComponent,
+    UserCartComponent
   ],
   imports: [
     BrowserModule,
